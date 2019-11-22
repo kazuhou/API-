08a8fe335641ee08aa6db2590d83eca84e7a67aa
kazuhou <hozuka.zero3zero8@gmail.com>
2019-11-22 17:11:04 +0900

    testcommit

---
+++ control/articles.php
@@ -0,0 +1,14 @@
+<?php
+ function get_articles(){
+  echo json_encode([
+    "message" => "get articles"
+  ]);
+ }
+
+ function get_articles(){
+  echo json_encode([
+    "message" => "post articles"
+  ]);
+ }
+
+
---
+++ env.php
@@ -0,0 +1,38 @@
+<?php
+// データベース
+const DB_SERVER_NAME = 'remotemysql.com';
+const USERNAME = 'LUcYNz1qg4';
+const PASSWORD = 'vnDYKMbPft';
+const DB_NAME = ' LUcYNz1qg4';
+// JWTで使う
+const SERVER_KEY = 'hogehoge';
+const ISSUER = 'hogehoge';
+
+
+// You have successfully created a new database. The details are below.
+
+// Username: Lz8qUIZmJL
+
+// Database name: Lz8qUIZmJL
+
+// Password: bl0Ag9VgDe
+
+// Server: remotemysql.com
+
+// Port: 3306
+
+// These are the username and password to log in to your database and phpMyAdmin
+
+// Make sure you keep your password secure. Ensure you keep back ups of your database in case it gets deleted.
+
+// Username: LUcYNz1qg4
+
+// Database name: LUcYNz1qg4
+
+// Password: vnDYKMbPft
+
+// Server: remotemysql.com
+
+// Port: 3306
+
+// These are the username and password to log in to your database and phpMyAdmin
